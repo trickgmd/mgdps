0368c28c9267e2aae1636d9e81a4f784f7176006
Minotaur256 <a2012agustin2@icloud.com>
2026-09-06 17:27:25 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -109,7 +109,7 @@ export default {
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
